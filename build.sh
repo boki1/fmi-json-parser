d0c843c5f1dae85c052a56a3192893abcd0e65a6
Kristiyan Stoimenov <kristoimenov@gmail.com>
2023-06-03 02:51:50 +0300

    build: Refactor build system and testing
    Signed-off-by: Kristiyan Stoimenov <kristoimenov@gmail.com>

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-# Use this script to build any singular library of the repository.
+# This is the project build script.
 # Pass --help to review how to use the script.
 
 output_dir=build
-skip_ccmake="no"
-ask="no"
+should_configure="yes"
+should_ask="no"
 should_clean="no"
 
 function help() {
@@ -13,44 +13,43 @@ function help() {
 }
 
 function build_procedure() {
-	conan install . --output-folder=${output_dir} -if=${output_dir} --build=missing
-	cmake -S. -B${output_dir} -GNinja -DCMAKE_EXPORT_COMPILE_COMMANDS=1
-	ln -sf build/compile_commands.json compile_commands.json
-	[ "${skip_ccmake}" == "no" ] && ccmake ${output_dir}
-	echo "configuration: $?"
+	[ "${should_configure}" == "no" ] || \
+		ccmake -S. -B${output_dir} -GNinja -DCMAKE_EXPORT_COMPILE_COMMANDS=1 -DCMAKE_BUILD_TYPE=Debug
+	[ -f ${output_dir}/compile_commands.json ] && \
+		ln -sf ${output_dir}/compile_commands.json compile_commands.json
 	ninja -C${output_dir}
 }
 
 function clean() {
-	question=$([ ${ask} == "yes" ] && echo "-i" || echo "")
-	rm -r ${question} ${output_dir}
+	question=$([ ${should_ask} == "yes" ] && echo "-i" || echo "")
+	rm -rf ${question} ${output_dir}
 }
 
 function parse_arguments() {
 	while [[ $# -gt 0 ]]; do
 		case "$1" in
-			--help)
-				help
-				exit
-				;;
-			--output=*)
-				output_dir="${1#*=}"
-				shift
-				;;
-			--skip-ccmake)
-				skip_ccmake="yes"
-				shift
-				;;
-			--clean)
-				should_clean="yes"
-				shift
-				[[ "$1" == "--ask" ]] && ask="yes"
-				clean
-				;;
-			*)
-				echo "$0: Invalid input."
-				exit
-				;;
+		--help)
+			help
+			exit
+			;;
+		--output=*)
+			output_dir="${1#*=}"
+			shift
+			;;
+		--configure=*)
+			should_configure="${1#*=}"
+			shift
+			;;
+		--clean)
+			should_clean="yes"
+			shift
+			[[ "$1" == "--ask" ]] && should_ask="yes"
+			clean
+			;;
+		*)
+			echo "$0: Invalid input."
+			exit
+			;;
 		esac
 	done
 }
