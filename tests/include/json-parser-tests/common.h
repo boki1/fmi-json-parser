e91e6f7d3aac0ac6e8b57576cb888c2b9a6735d0
Kristiyan Stoimenov <kristoimenov@gmail.com>
2023-06-22 03:15:58 +0300

    lib+tests+ignore: Add reprinting as a testing procedure
    Implement a unit test which performs the following:
     - Read a sample JSON file and parse it.
     - The parsed object gets serialized to a file.
     - The resulting file is then again parsed.
     - The newly parsed object gets serialized to another file.
     - The two output files (*) and (**) are compared. It is expected that
     	they are identical.
    
    During the implementation of this multiple problems with the current
    "serialize()" implementation for the different "json::*" types were
    identified - they are now fixed and printing looks great :).
    
    Signed-off-by: Kristiyan Stoimenov <kristoimenov@gmail.com>

--- tests/include/json-parser-tests/common.h
+++ tests/include/json-parser-tests/common.h
@@ -3,6 +3,11 @@
 
 #include <string>
 #include <fstream>
+#include <utility>
+#include <filesystem>
+namespace fs = std::filesystem;
+
+#include <json-parser/parser.h>
 
 #ifndef TESTS_DIR_PREFIX
 #define TESTS_DIR_PREFIX "tests/"
@@ -14,6 +19,38 @@ std::string slurp(const std::string &filename) {
 
     std::ifstream in{filename};
     return std::string{std::istreambuf_iterator<char>{in}, {}};
+}
+
+std::string file_prefix(const std::string &sample_name) {
+    return fs::path{sample_name}.filename().replace_extension("");
+}
+
+// This is used for the test_reprint test cases.
+// It follows a simple procedure which seems effective.
+//
+// 1. Parse a sample "A" and print it to a file "B".
+// 2. Parse file "B" and print it to file "C".
+// 3. Make sure that "B" and "C" are the same.
+//
+// Also this is the one used in the nlohman JSON test cases, so
+// I consider it to be a not-bad idea :D.
+std::pair<std::string, std::string> reprint(const std::string &sample_name) {
+    using namespace json_parser;
+
+    const std::string out_dir = TESTS_DIR_PREFIX"reprints/" + file_prefix(sample_name) + "/";
+    fs::create_directories(out_dir);
+
+    const json parsed1 = parser{sample_name}();
+    const std::string out1 = out_dir + "out1.json";
+    std::ofstream out1_ifs{out1, std::ios::trunc | std::ios::out};
+    parsed1.dump(out1_ifs);
+
+    const json parsed2 = parser{out1}();
+    const std::string out2 = out_dir + "out2.json";
+    std::ofstream out2_ifs{out2, std::ios::trunc | std::ios::out};
+    parsed1.dump(out2_ifs);
+
+    return std::make_pair(slurp(out1), slurp(out2));
 }
 
 #endif // FMI_JSON_PARSER_TESTS_COMMON_INCLUDED
---
+++ tests/test_reprint.cpp
@@ -0,0 +1,27 @@
+#include <filesystem>
+namespace fs = std::filesystem;
+
+#include <gtest/gtest.h>
+
+#include <json-parser/parser.h>
+
+#include <json-parser-tests/common.h>
+
+using namespace json_parser;
+
+void reprint_cmp(const std::string &sample_name) {
+    auto [print1, print2] = reprint(std::string{TESTS_DIR_PREFIX} + sample_name);
+    EXPECT_EQ(print1, print2);
+}
+
+// We try this test case procedure only on the sane JSON inputs as
+// if the input is not correct it won't be parsed fully.
+TEST(JsonTests, Reprint) {
+    reprint_cmp("samples/empty.json");
+    reprint_cmp("samples/string-only.json");
+    reprint_cmp("samples/array.json");
+    reprint_cmp("samples/simple.json");
+    reprint_cmp("samples/jokes.json");
+    reprint_cmp("samples/nested.json");
+    reprint_cmp("samples/array_of_objects.json");
+}
